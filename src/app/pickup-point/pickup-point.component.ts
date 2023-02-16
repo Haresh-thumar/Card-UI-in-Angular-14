a439d84e010d52f4e903179939981abc01f6e822
Haresh-thumar <hareshthumar46@gmail.com>
2023-02-16 16:56:44 +0530

    variable name changes in timezone logic

--- src/app/pickup-point/pickup-point.component.ts
+++ src/app/pickup-point/pickup-point.component.ts
@@ -16,7 +16,7 @@ export class PickupPointComponent implements OnInit {
     this.getdiff();
   }
 
-  timeObject: any[] = [
+  busRouteObj: any[] = [
     { id: '5fs61f1f56s1bsb6s546', startTime: '2015-01-01T07:05:00.000Z', endTime: '2015-01-01T08:45:00.000Z', differentTime: '' },
     { id: '2f965fbfbdf6b6fd5b6f', startTime: '2015-01-01T07:30:00.000Z', endTime: '2015-01-01T15:15:00.000Z', differentTime: '' },
     { id: '9b564b64b65d561dsv5d', startTime: '2015-01-01T06:50:00.000Z', endTime: '2015-01-01T08:20:00.000Z', differentTime: '' },
@@ -26,30 +26,31 @@ export class PickupPointComponent implements OnInit {
     { id: '9fsa4c8s64c64sc6sa5c', startTime: '2015-01-01T08:23:00.000Z', endTime: '2015-01-01T09:19:00.000Z', differentTime: '' },
     { id: '3as8c4s94c86s48c64ps', startTime: '2015-01-01T06:38:00.000Z', endTime: '2015-01-01T08:54:00.000Z', differentTime: '' },
     { id: '1sdc4864c64c646c465y', startTime: '2015-01-01T08:18:00.000Z', endTime: '2015-01-01T09:39:00.000Z', differentTime: '' },
-    { id: '7bd68v486464dv648d6v', startTime: '2015-01-01T05:55:00.000Z', endTime: '2015-01-01T08:30:00.000Z', differentTime: '' },
+    { id: '7bd68v486464dv648d6v', startTime: '2015-01-01T06:00:00.000Z', endTime: '2015-01-01T08:00:00.000Z', differentTime: '' },
   ];
 
   /*--------- Using Object Value to Get Two Timezone Difference Complete Working Method ----------*/
   getdiff() {
     console.clear();
-    for (let time of this.timeObject) {
+    for (let getTime of this.busRouteObj) {
 
-      let dt1 = time.startTime;
-      let dt2 = time.endTime;
+      let busStartTime = getTime.startTime;
+      let busEndTime = getTime.endTime;
 
-      let start = moment(dt1);
-      let end = moment(dt2);
+      let startTime = moment(busStartTime);
+      let endTime = moment(busEndTime);
 
       /*---- Getting the difference: hours (h), minutes (m) and seconds (s) ----*/
-      let h = end.diff(start, 'hours');
-      let m = end.diff(start, 'minutes') - (60 * h);
+      let hour = endTime.diff(startTime, 'hours');
+      let min = endTime.diff(startTime, 'minutes') - (60 * hour);
 
       /*---- Formating in hh:mm:ss (appends a left zero when num < 10) ----*/
-      let hh = ('0' + h).slice(-2);
-      let mm = ('0' + m).slice(-2);
+      let hh = ('0' + hour).slice(-2);
+      let mm = ('0' + min).slice(-2);
 
-      let hhh = +hh > 0 ? hh + " hr" : "";
-      console.log(`${hhh} ${mm} min`);
+      let finalHour = +hh > 0 ? hh + " hr" : "";
+      let finalMin = mm > '00' ? mm + " min" : "";
+      console.log(`${finalHour} ${finalMin}`);
 
     }
   }
