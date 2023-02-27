578036e942d38e2d3bc19ccbe1e1fa1260a97097
Haresh-thumar <hareshthumar46@gmail.com>
2023-02-27 10:08:03 +0530

    ui design

--- src/app/get-timezone-diff/get-timezone-diff.component.ts
+++ src/app/get-timezone-diff/get-timezone-diff.component.ts
@@ -17,6 +17,9 @@ export class GetTimezoneDiffComponent implements OnInit {
 
     /*----- for Get Sum for Nested Objects -----*/
     this.totalStudentCount();
+
+    /*----- for sort time wise object ----*/
+    this.sortPickTime();
   }
 
 
@@ -80,12 +83,12 @@ export class GetTimezoneDiffComponent implements OnInit {
       name: '101-A',
       totalStudent: 0,
       pickPoints: [
-        { pickPoinId: '5b0814d9889c', areaName: 'aaa', studentLength: 1 },
-        { pickPoinId: 'a0e8e79b5c31', areaName: 'bbb', studentLength: 3 },
-        { pickPoinId: '5bad393db396', areaName: 'ccc', studentLength: 5 },
-        { pickPoinId: '946eee1a904c', areaName: 'ddd', studentLength: 7 },
-        { pickPoinId: '65159d41e2fc', areaName: 'eee', studentLength: 9 },
-        { pickPoinId: '2039756dc403', areaName: 'fff', studentLength: 11 },
+        { pickPoinId: '5b0814d9889c', pickTime: '2023-02-16T01:45:18Z', areaName: 'aaa', studentLength: 1 },
+        { pickPoinId: 'a0e8e79b5c31', pickTime: '2023-02-16T02:00:10Z', areaName: 'bbb', studentLength: 3 },
+        { pickPoinId: '5bad393db396', pickTime: '2023-02-16T01:30:55Z', areaName: 'ccc', studentLength: 5 },
+        { pickPoinId: '946eee1a904c', pickTime: '2023-02-16T02:00:35Z', areaName: 'ddd', studentLength: 7 },
+        { pickPoinId: '65159d41e2fc', pickTime: '2023-02-16T02:30:09Z', areaName: 'eee', studentLength: 9 },
+        { pickPoinId: '2039756dc403', pickTime: '2023-02-17T01:05:55Z', areaName: 'fff', studentLength: 11 },
       ]
     },
     {
@@ -93,12 +96,12 @@ export class GetTimezoneDiffComponent implements OnInit {
       name: '101-B',
       totalStudent: 0,
       pickPoints: [
-        { pickPoinId: '358273123cc2', areaName: 'ggg', studentLength: 2 },
-        { pickPoinId: '01cf09c64f89', areaName: 'hhh', studentLength: 4 },
-        { pickPoinId: 'f88c0e91b16f', areaName: 'iii', studentLength: 6 },
-        { pickPoinId: 'aa7d9917bebc', areaName: 'jjj', studentLength: 8 },
-        { pickPoinId: '6368189811dc', areaName: 'kkk', studentLength: 10 },
-        { pickPoinId: '980a26427a7f', areaName: 'lll', studentLength: 12 },
+        { pickPoinId: '358273123cc2', pickTime: '2023-02-17T01:45:16Z', areaName: 'ggg', studentLength: 2 },
+        { pickPoinId: '01cf09c64f89', pickTime: '2023-02-17T02:00:30Z', areaName: 'hhh', studentLength: 4 },
+        { pickPoinId: 'f88c0e91b16f', pickTime: '2023-02-21T01:37:47Z', areaName: 'iii', studentLength: 6 },
+        { pickPoinId: 'aa7d9917bebc', pickTime: '2023-02-21T01:40:03Z', areaName: 'jjj', studentLength: 8 },
+        { pickPoinId: '6368189811dc', pickTime: '2023-02-20T01:28:14Z', areaName: 'kkk', studentLength: 10 },
+        { pickPoinId: '980a26427a7f', pickTime: '2023-02-20T01:30:34Z', areaName: 'lll', studentLength: 12 },
       ],
     },
     {
@@ -106,12 +109,12 @@ export class GetTimezoneDiffComponent implements OnInit {
       name: '101-C',
       totalStudent: 0,
       pickPoints: [
-        { pickPoinId: 'b12c54e28d96', areaName: 'mmm', studentLength: 3 },
-        { pickPoinId: '50253993ec90', areaName: 'nnn', studentLength: 6 },
-        { pickPoinId: '0f88f3a965af', areaName: 'ooo', studentLength: 9 },
-        { pickPoinId: '526e815ac45e', areaName: 'ppp', studentLength: 12 },
-        { pickPoinId: '5507c6a40bdf', areaName: 'qqq', studentLength: 15 },
-        { pickPoinId: '082b9b6efc81', areaName: 'rrr', studentLength: 18 },
+        { pickPoinId: 'b12c54e28d96', pickTime: '2023-02-20T02:15:16Z', areaName: 'mmm', studentLength: 3 },
+        { pickPoinId: '50253993ec90', pickTime: '2023-02-21T01:36:39Z', areaName: 'nnn', studentLength: 6 },
+        { pickPoinId: '0f88f3a965af', pickTime: '2023-02-20T01:52:03Z', areaName: 'ooo', studentLength: 9 },
+        { pickPoinId: '526e815ac45e', pickTime: '2023-02-21T01:37:20Z', areaName: 'ppp', studentLength: 12 },
+        { pickPoinId: '5507c6a40bdf', pickTime: '2023-02-20T02:30:10Z', areaName: 'qqq', studentLength: 15 },
+        { pickPoinId: '082b9b6efc81', pickTime: '2023-02-20T02:00:26Z', areaName: 'rrr', studentLength: 18 },
       ],
     },
     {
@@ -119,12 +122,12 @@ export class GetTimezoneDiffComponent implements OnInit {
       name: '101-D',
       totalStudent: 0,
       pickPoints: [
-        { pickPoinId: '8503fe9f47a8', areaName: 'sss', studentLength: 4 },
-        { pickPoinId: 'c5db3b970dda', areaName: 'ttt', studentLength: 8 },
-        { pickPoinId: 'fcf6071094e0', areaName: 'uuu', studentLength: 12 },
-        { pickPoinId: '61fc49d30f34', areaName: 'vvv', studentLength: 16 },
-        { pickPoinId: 'c6ee9b741dc4', areaName: 'www', studentLength: 20 },
-        { pickPoinId: '7a8f38418aaa', areaName: 'xxx', studentLength: 24 },
+        { pickPoinId: '8503fe9f47a8', pickTime: '2023-02-17T02:00:30Z', areaName: 'sss', studentLength: 4 },
+        { pickPoinId: 'c5db3b970dda', pickTime: '2023-02-21T01:37:47Z', areaName: 'ttt', studentLength: 8 },
+        { pickPoinId: 'fcf6071094e0', pickTime: '2023-02-20T02:00:26Z', areaName: 'uuu', studentLength: 12 },
+        { pickPoinId: '61fc49d30f34', pickTime: '2023-02-21T01:36:39Z', areaName: 'vvv', studentLength: 16 },
+        { pickPoinId: 'c6ee9b741dc4', pickTime: '2023-02-20T02:15:16Z', areaName: 'www', studentLength: 20 },
+        { pickPoinId: '7a8f38418aaa', pickTime: '2023-02-20T01:30:34Z', areaName: 'xxx', studentLength: 24 },
       ],
     },
     {
@@ -132,24 +135,39 @@ export class GetTimezoneDiffComponent implements OnInit {
       name: '101-E',
       totalStudent: 0,
       pickPoints: [
-        { pickPoinId: '429f867fdaaa', areaName: 'yyy', studentLength: 5 },
-        { pickPoinId: '95c4d4cb56a9', areaName: 'zzz', studentLength: 10 },
-        { pickPoinId: 'f323f7f821d7', areaName: 'abc', studentLength: 15 },
-        { pickPoinId: '2da748c10c33', areaName: 'def', studentLength: 20 },
-        { pickPoinId: '5a32e4ea49de', areaName: 'ghi', studentLength: 25 },
-        { pickPoinId: 'db9612b4f4bb', areaName: 'jkl', studentLength: 30 },
+        { pickPoinId: '429f867fdaaa', pickTime: '2023-02-16T01:45:18Z', areaName: 'yyy', studentLength: 5 },
+        { pickPoinId: '95c4d4cb56a9', pickTime: '2023-02-16T02:00:10Z', areaName: 'zzz', studentLength: 10 },
+        { pickPoinId: 'f323f7f821d7', pickTime: '2023-02-17T03:20:30Z', areaName: 'abc', studentLength: 15 },
+        { pickPoinId: '2da748c10c33', pickTime: '2023-02-21T01:37:47Z', areaName: 'def', studentLength: 20 },
+        { pickPoinId: '5a32e4ea49de', pickTime: '2023-02-21T01:36:39Z', areaName: 'ghi', studentLength: 25 },
+        { pickPoinId: 'db9612b4f4bb', pickTime: '2023-02-20T02:15:16Z', areaName: 'jkl', studentLength: 30 },
       ],
     },
   ];
 
   /*--- get sum using method ---*/
   totalStudentCount() {
-    const res = this.sumNestedObject.map(arr => {
-      arr.pickPoints.forEach((obj: any) => {
-        arr.totalStudent += obj.studentLength;
+    const resp = this.sumNestedObject.map(arr => {
+      arr.pickPoints.forEach((res: any) => {
+        arr.totalStudent += res.studentLength;
+        console.log(arr.totalStudent);
       });
-      return [arr.totalStudent];
+      // return [arr.totalStudent];
     });
+  }
+
+
+  /*--- get sum using method ---*/
+  sortPickTime() {
+    for (let mainOjb of this.sumNestedObject) {
+      // mainOjb.pickPoints.sort((a: any, b: any) => (a.pickTime > b.pickTime) ? 1 : (a.pickTime < b.pickTime) ? -1 : 0);
+      console.log(mainOjb);
+
+      for (let innerObj of mainOjb.pickPoints) {
+        let aaa = Object.values(innerObj)[1];
+        console.warn(aaa)
+      }
+    }
   }
 
 
